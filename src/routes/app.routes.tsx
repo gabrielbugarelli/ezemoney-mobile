748efda024ae756a75dd2a8465b6887bf52d383d
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-03-27 15:15:49 -0400

    feat: estilizando a Tab Navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 
 import { Dashboard } from '../screens/dashboard';
 import { Register } from '../screens/register';
@@ -6,11 +8,59 @@ import { Register } from '../screens/register';
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export const AppRoutes = () => {
+  const theme = useTheme();
+
   return (
-    <Navigator>
-      <Screen name="listagem" component={Dashboard} />
-      <Screen name="cadastrar" component={Register} />
-      <Screen name="resumo" component={Register} />
+    <Navigator screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: theme.colors.primary,
+      tabBarLabelPosition: 'beside-icon',
+      tabBarStyle: {
+        height: 65
+      }
+    }} >
+
+      <Screen 
+        name="Transações"
+        component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons 
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+
+      <Screen 
+        name="Lançamentos" 
+        component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <MaterialIcons 
+              name="attach-money"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+      
+      <Screen 
+        name="Métricas"
+        component={Register}
+        options={{
+          tabBarIcon: (({size, color}) =>
+            <MaterialIcons
+              name="pie-chart"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
     </Navigator>
   )
 }
